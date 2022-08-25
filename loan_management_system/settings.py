c0fc5e5598e5ae470e0f0b16c4a45763e700b651
vishruth10 <>
2022-08-25 23:03:35 +0530

    small changes

--- loan_management_system/settings.py
+++ loan_management_system/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.views.decorators.csrf import csrf_exempt
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
