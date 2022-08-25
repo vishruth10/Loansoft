18e3be970806b09c3ac20e2535920c9a4272af78
vishruth10 <>
2022-08-25 22:56:33 +0530

    * removed

--- loan_management_system/settings.py
+++ loan_management_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@^8mr&i%+n@g(%x67d1ry*%929yvveyf$1_5$vt2w6f=hl*qnp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*","localhost","loansoftmit.herokuapp.com","chrome-extension://nmmhkkegccagdldgiimedpiccmgmieda"]
+ALLOWED_HOSTS = ["localhost","loansoftmit.herokuapp.com","chrome-extension://nmmhkkegccagdldgiimedpiccmgmieda"]
 
 
 # Application definition
