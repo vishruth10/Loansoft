ab902c39f0d90b44b8d5c8773238d4c138adace5
vishruth10 <>
2022-08-26 10:32:51 +0530

    added new extension

--- loan_management_system/settings.py
+++ loan_management_system/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-#from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-@^8mr&i%+n@g(%x67d1ry*%929yvveyf$1_5$vt2w6f=hl*qnp'
+#SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@^8mr&i%+n@g(%x67d1ry*%929yvveyf$1_5$vt2w6f=hl*qnp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
