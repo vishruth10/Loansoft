11d9aecda146a32eee563e93c23037685a0bfaff
vishruth10 <>
2022-08-26 20:48:13 +0530

    final

--- loan_management_system/settings.py
+++ loan_management_system/settings.py
@@ -137,7 +137,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 WHITENOISE_USE_FINDERS = True
-# Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
