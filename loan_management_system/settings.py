b073cdc5170a398519b1d74f21cd3f5942c9b132
vishruth10 <>
2022-08-26 20:42:02 +0530

    import dj

--- loan_management_system/settings.py
+++ loan_management_system/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from django.views.decorators.csrf import csrf_exempt
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,7 +95,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
     
